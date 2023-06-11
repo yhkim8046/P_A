7d87fb8e701c30792055e4f03cdd5d87cd899159
yhkim8046 <82135063+yhkim8046@users.noreply.github.com>
2023-06-12 04:32:45 +1200

    Version 1.1
    Function added to select location and hotels in search mode

---
+++ PDC/JPanel.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PDC;
+
+import java.awt.GridLayout;
+
+/**
+ *
+ * @author yuhwankim
+ */
+public class JPanel {
+
+    public JPanel() {
+    }
+
+    void setBounds(int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setLayout(GridLayout gridLayout) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
--- PDC/SearchFrame.java
+++ PDC/SearchFrame.java
@@ -4,9 +4,10 @@
  */
 package PDC;
 
-import static PDC.AppFrame.height;
-import static PDC.AppFrame.width;
+import java.awt.GridLayout;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel; 
 
 /**
  *
@@ -14,18 +15,47 @@ import javax.swing.JFrame;
  */
 public class SearchFrame extends JFrame
 {
-    private int height = 720;
-    private int width = 720;
+    private int height = 400;
+    private int width = 400;
+    private JComboBox locationComboBox;
+    private Auckland au;
+    private JPanel panel;
+    private JComboBox hotelComboBox;
     
     public SearchFrame()
     {
+        String[] hotels = {"Auckland", "ChristChurch"};
+        
         this.setTitle("Searching");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         this.setSize(width,height);
         this.setResizable(false);
         setLocationRelativeTo(null);
-        this.setVisible(true);
+        
         this.setLayout(null);
+        
+        locationComboBox = new JComboBox(hotels);
+        locationComboBox.setBounds(100, 0, 200, 50);
+        this.add(locationComboBox);
+        
+        panel = new JPanel();
+        panel.setBounds(100, 100, 300, 100);
+        panel.setLayout(new GridLayout());
+        
+        au = new Auckland();
+        au.displayHotels(panel);
+        
+        this.add(panel);
+        
+        
+        hotelComboBox = new JComboBox<>();
+        hotelComboBox.setBounds(100, 250, 200, 50);
+        this.add(hotelComboBox);
+        
+        // Populate hotel names into the combo box
+        for (Hotel hotel : au.getAucklandHotels()) {
+            hotelComboBox.addItem(hotel.getName());
+        }
     }
 }
